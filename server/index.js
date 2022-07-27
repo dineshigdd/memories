de4cf8130af3d699db0170512e7f357171125dba
unknown <dineshigdd@gmail.com>
2022-07-26 23:09:31 -0700

    add routes and front-end files

--- server/index.js
+++ server/index.js
@@ -2,8 +2,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import postRouter from './routes/post.js';
 
 const app = express();
+
+app.use('/posts', postRouter);
 
 app.use( bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
