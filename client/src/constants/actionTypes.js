c3c5f3a6dfc60345b4fbb8bd061d9e68b620b1fe
unknown <dineshigdd@gmail.com>
2022-11-02 20:06:53 -0700

    add constants

---
+++ client/src/constants/actionTypes.js
@@ -0,0 +1,5 @@
+export const FETCH_ALL = "FETCH-ALL"
+export const CREATE = "CREATE"
+export const UPDATE = "UPDATE"
+export const UPDATE_LIKE_COUNT = "UPDATE_LIKE_COUNT"
+export const DELETE = "DELETE"
